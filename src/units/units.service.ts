39bfcac79bcf8cd32b39222efc99ce2d0603752b
Lucas Previato <lucaspreviato@live.com>
2022-12-04 23:19:23 -0300

    feat(unit): was implemented all services for unit
    created endpoint for creation unit, findall,findone, update and remove and creation error exception
    customized

--- src/units/units.service.ts
+++ src/units/units.service.ts
@@ -1,26 +1,45 @@
+import { EntityRepository, wrap } from '@mikro-orm/core';
+import { InjectRepository } from '@mikro-orm/nestjs';
 import { Injectable } from '@nestjs/common';
 import { CreateUnitInput } from './dto/create-unit.input';
 import { UpdateUnitInput } from './dto/update-unit.input';
+import { Unit } from './entities/unit.entity';
+import PostNotFoundException from './errors/not-found-exception';
 
 @Injectable()
 export class UnitsService {
-  create(createUnitInput: CreateUnitInput) {
-    return 'This action adds a new unit';
+  constructor(
+    @InjectRepository(Unit)
+    private readonly unitRepository: EntityRepository<Unit>,
+  ) {}
+  async create({ name, email, phone }: CreateUnitInput) {
+    const newUnit = await this.unitRepository.create({ name, email, phone });
+    await this.unitRepository.persistAndFlush(newUnit);
+    return newUnit;
   }
 
-  findAll() {
-    return `This action returns all units`;
+  async findAll(): Promise<Unit[]> {
+    const units = await this.unitRepository.findAll();
+    return units;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} unit`;
+  async findOne(id: number): Promise<Unit> {
+    const unit = await this.unitRepository.findOne(id);
+    if (!unit) {
+      throw new PostNotFoundException(id);
+    }
+    return unit;
   }
 
-  update(id: number, updateUnitInput: UpdateUnitInput) {
-    return `This action updates a #${id} unit`;
+  async update(id: number, { email, name, phone }: UpdateUnitInput) {
+    const existingUnit = await this.unitRepository.findOne(id);
+    wrap(existingUnit).assign({ email, name, phone });
+    await this.unitRepository.persistAndFlush(existingUnit);
+    return existingUnit;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} unit`;
+  async remove(id: number) {
+    const unit = await this.unitRepository.findOne(id);
+    this.unitRepository.removeAndFlush(unit);
   }
 }
