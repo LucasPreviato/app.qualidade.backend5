c8ac0a8cbb0b632cdb7eac4325dd5d677dc68dc0
Lucas Previato <lucaspreviato@live.com>
2022-12-07 14:37:34 -0300

    feat(departments): implements entity class

--- src/departments/departments.module.ts
+++ src/departments/departments.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { DepartmentsService } from './departments.service';
 import { DepartmentsResolver } from './departments.resolver';
+import { Department } from './entities/department.entity';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { UnitsModule } from 'src/units/units.module';
+import { Unit } from './../units/entities/unit.entity';
 
 @Module({
+  imports: [
+    MikroOrmModule.forFeature({ entities: [Department, Unit] }),
+    UnitsModule,
+  ],
   providers: [DepartmentsResolver, DepartmentsService],
 })
 export class DepartmentsModule {}
--- src/departments/entities/department.entity.ts
+++ src/departments/entities/department.entity.ts
@@ -1,7 +1,29 @@
+import { Entity, ManyToOne, PrimaryKey, Property } from '@mikro-orm/core';
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Unit } from './../../units/entities/unit.entity';
 
 @ObjectType()
+@Entity()
 export class Department {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int)
+  @PrimaryKey()
+  id: number;
+  @Field()
+  @Property({ unique: true })
+  name: string;
+  @Field({ nullable: true })
+  @Property()
+  description?: string;
+  @Field(() => Date)
+  @Property()
+  createdAt: Date;
+  @Field({ nullable: true })
+  @Property()
+  email?: string;
+  @Field()
+  @Property({ unique: true })
+  initials: string;
+  @Field(() => Unit)
+  @ManyToOne()
+  unitid: Unit;
 }
